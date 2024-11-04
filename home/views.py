bfc0874233b845fc71b93898ad83694cdf6cf331
Josimarmn <mnjosimar@gmail.com>
2024-11-04 20:09:31 -0300

    atividade dia 26/10

---
+++ home/views.py
@@ -0,0 +1,12 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request,'index.html')
+
+def sobre(request):
+    return render(request,'sobre.html')
+
+def contato(request):
+    return render(request,'contato.html')
